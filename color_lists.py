ef81e56e06acb0b70cddcd6150c87374aa378dbf
orangethecat <ayseoztekin0619@gmail.com>
2025-09-30 01:26:44 +0300

    Create color_lists.py

---
+++ color_lists.py
@@ -0,0 +1,2 @@
+color_list = [(240, 246, 242), (239, 242, 247), (198, 165, 116), (144, 79, 55), (221, 201, 138), (58, 93, 121), (167, 153, 48), (132, 34, 23)]
+colors_list = ["AliceBlue", "BlueViolet", "blue2","chartreuse2","CornflowerBlue","cyan4", "DarkBlue", "DarkCyan","DarkGoldenrod","DarkGray","DarkGreen","DeepSkyBlue2","DeepSkyBlue4","gray80","HotPink3","IndianRed"]
